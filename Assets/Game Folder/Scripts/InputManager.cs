81a8e0b37c19692f75dba5094a2d8fe08a8bff3e
Josh van Asten <joshmathew.va@gmail.com>
2016-11-16 22:09:15 +0100

    + Added InputManager axes for mouse and joystick camera movement

--- Assets/Game Folder/Scripts/InputManager.cs
+++ Assets/Game Folder/Scripts/InputManager.cs
@@ -25,6 +25,28 @@ public static class InputManager
         return new Vector3(MovementHorizontal(), 0, MovementVertical());
     }
 
+    //Mouse camera movement
+    public static float CameraHorizontal()
+    {
+        float result = 0.0f;
+        result += Input.GetAxis("J_CameraHorizontal");
+        result += Input.GetAxis("M_CameraHorizontal");
+        return Mathf.Clamp(result, -1.0f, 1.0f);
+    }
+
+    public static float CameraVertical()
+    {
+        float result = 0.0f;
+        result += Input.GetAxis("J_CameraVertical");
+        result += Input.GetAxis("M_CameraVertical");
+        return Mathf.Clamp(result, -1.0f, 1.0f);
+    }
+
+    public static Vector3 CameraMovement()
+    {
+        return new Vector3(CameraHorizontal(), 0, CameraVertical());
+    }
+
     //Triggers / ColumnControl
     public static float RaiseColumn()
     {
