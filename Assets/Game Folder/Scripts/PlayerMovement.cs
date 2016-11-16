dfefde2bee7e00bb6bd634eee6090d9605bb7788
Dominick <d.meels@hotmail.com>
2016-11-16 22:03:55 +0100

    + Added 3 scripts, Ball, PlayerCamera, PlayerMovement o Changed Dominick - Testbench scene

---
+++ Assets/Game Folder/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Could be changed for an enum later on, for now we can use a Boolean
+    private bool _ballPosession = false;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+}
