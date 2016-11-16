93cd07921e752899d40a1573b93629b09555879e
Josh van Asten <joshmathew.va@gmail.com>
2016-11-16 19:27:00 +0100

    o Minor changes to ColumnControl and the Greybox scene

--- Assets/Game Folder/Scripts/ColumnControl.cs
+++ Assets/Game Folder/Scripts/ColumnControl.cs
@@ -10,22 +10,20 @@ public class ColumnControl : MonoBehaviour
     private float columnDisplacementSize;
 
     [SerializeField]
-    private float columnMovementSpeed;
+    private float columnMovementMaxSpeed;
 
     private GameObject selectedColumn;
     private float baseYValue;
     private bool columnRising;
     private bool columnLowering;
-    private bool columnMoving; //is the column moving at all
 
     // Use this for initialization
     void Start()
     {
         columnRising = false;
         columnLowering = false;
-        columnMoving = false;
 
-        baseYValue = unmovingColumn.transform.position.y;
+        baseYValue = unmovingColumn.transform.position.y;      
     }
 
     // Update is called once per frame
@@ -35,6 +33,7 @@ public class ColumnControl : MonoBehaviour
         detectColumnControlInput();
         updateColumnPosition();
         isColumnMoving();
+        Debug.Log("columnMoving: " + isColumnMoving() + ", columnRising: " + columnRising + ", columnLowering: " + columnLowering);
     }
 
     //handles the raycast selecting the right column
@@ -43,7 +42,7 @@ public class ColumnControl : MonoBehaviour
         RaycastHit raycastHit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        if (Physics.Raycast(ray, out raycastHit) && columnMoving == false)
+        if (Physics.Raycast(ray, out raycastHit) && isColumnMoving() == false)
         {
             selectedColumn = raycastHit.collider.gameObject;
         }
@@ -52,11 +51,11 @@ public class ColumnControl : MonoBehaviour
     //wait for imput from lowering / raising column buttons
     private void detectColumnControlInput()
     {
-        if (InputManager.RaiseColumn() > 0 && selectedColumn != null && columnMoving == false)
+        if (InputManager.RaiseColumn() > 0 && selectedColumn != null && isColumnMoving() == false)
         {
             columnRising = true;
         }
-        if (InputManager.LowerColumn() > 0 && selectedColumn != null && columnMoving == false)
+        if (InputManager.LowerColumn() > 0 && selectedColumn != null && isColumnMoving() == false)
         {
             columnLowering = true;
         }
@@ -75,40 +74,37 @@ public class ColumnControl : MonoBehaviour
         }
     }
 
-    private void isColumnMoving()
+    private bool isColumnMoving()
     {
         //if the column is moving in any direction mark it as moving
         if (columnRising == true || columnLowering == true)
         {
-            columnMoving = true;
+            return true;
         }
         else
         {
-            columnMoving = false;
+            return false;
         }
+    }
 
-        //if the column gets called to stop, make sure both bools also stop
-        if (columnMoving == false)
-        {
-            columnRising = false;
-            columnLowering = false;
-        }
+    private void columnHalted()
+    {
+        columnRising = false;
+        columnLowering = false;
     }
 
     //actually move the column
     private void moveColumn(float pPolarity)
     {
         //if column is not yet at the height of it's end position
-        if (selectedColumn.transform.position.y < unmovingColumn.transform.position.y + (pPolarity * columnDisplacementSize))
+        if (selectedColumn.transform.position.y < pPolarity * (unmovingColumn.transform.position.y + (pPolarity * columnDisplacementSize)) && isColumnMoving() == true)
         {
-            selectedColumn.transform.Translate(0, pPolarity * columnMovementSpeed, 0);
-            Debug.Log("MOVING " + columnMovementSpeed + " amount(" + pPolarity + " / " + pPolarity * columnMovementSpeed + ")");
-            Debug.Log("selected column y: " + selectedColumn.transform.position.y);
+            selectedColumn.transform.Translate(0, pPolarity * columnMovementMaxSpeed, 0);
         }
         else
         {
             //stop the column moving, which should deactive both columnRising and columnLowering
-            columnMoving = false;
+            columnHalted();
         }
     }
 }
