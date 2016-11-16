c417203ff332fcb3bc9673c6bf12ba9f57d61846
Dominick <d.meels@hotmail.com>
2016-11-17 00:41:39 +0100

    + PlayerInput script o Changed PlayerMovement script o Changed Dominick - Testbench scene

---
+++ Assets/Game Folder/Scripts/PlayerInput.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour
+{
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+}
--- Assets/Game Folder/Scripts/PlayerMovement.cs
+++ Assets/Game Folder/Scripts/PlayerMovement.cs
@@ -3,16 +3,29 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField]
+    private float _movementSpeed;
+
     //Could be changed for an enum later on, for now we can use a Boolean
     private bool _ballPosession = false;
 
+    private Rigidbody _rigidBody;
+
     private void Start()
     {
-
+        _rigidBody = GetComponent<Rigidbody>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
+        Move();
+    }
 
+    public void Move()
+    {
+        if(_rigidBody.velocity.magnitude < _movementSpeed)
+        {
+            _rigidBody.AddForce(InputManager.Movement() * 10);
+        }
     }
 }
