0b7a52440e2b3dd12cc5ec227d43b6b020c86868
Dominick <d.meels@hotmail.com>
2016-11-19 00:10:25 +0100

    o Changed Dominick - Testbench o Changed Ball script + added a Prefabs folder

--- Assets/Game Folder/Scripts/Ball.cs
+++ Assets/Game Folder/Scripts/Ball.cs
@@ -7,7 +7,7 @@ public class Ball : MonoBehaviour
 
     private void Start()
     {
-        _rigidbody = GetComponent<Rigidbody>();
+       // _rigidbody = GetComponent<Rigidbody>();
     }
 
     private void Update()
@@ -17,12 +17,20 @@ public class Ball : MonoBehaviour
 
     private void OnCollisionEnter(Collision pCollision)
     {
+        PlayerMovement movement = pCollision.gameObject.GetComponent<PlayerMovement>();
+        if(movement != null)
+        {
+            transform.SetParent(movement.transform);
+            changePlayerState(movement);
+            //_rigidbody.velocity = Vector3.zero;
+            transform.localPosition = new Vector3(0, 1, 0);
 
+        }
     }
 
     private void changePlayerState(PlayerMovement pMovement)
     {
-
+        print("HELLO ITS A ME MARIO");
     }
 
     private void score()
