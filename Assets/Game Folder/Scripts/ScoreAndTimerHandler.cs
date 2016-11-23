2ac31e8114034cdc85d35f403537de8fde8aa5ab
Josh van Asten <joshmathew.va@gmail.com>
2016-11-23 22:55:14 +0100

    o Renamed "ScoreAndTimerHandelr" to "ScoreAndTimerHandler" considering it got deleted anyways o Attached it onto the HUD - Removed "Time: " and "Score: " from the ScoreAndTimerHandler as we have official UI now

---
+++ Assets/Game Folder/Scripts/ScoreAndTimerHandler.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreAndTimerHandler : MonoBehaviour
+{
+    [SerializeField]
+    private Text _team1_scoreText;
+    [SerializeField]
+    private Text _team2_scoreText;
+    [SerializeField]
+    private Text _timerText;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        _team1_scoreText.text = "" + MatchStatistics.GetGoalsForTeam(1).ToString();
+        _team2_scoreText.text = "" + MatchStatistics.GetGoalsForTeam(2).ToString();
+        //_timerText.text = "Time: " + ((int)Time.time).ToString();
+        _timerText.text = transform2Clock((int)Time.time);
+    }
+
+    private string transform2Clock(int pTime)
+    {
+        int minutes = pTime / 60;
+        int seconds = pTime % 60;
+
+        string minutesString;
+        string secondsString;
+
+        if (minutes < 10) { minutesString = "0" + minutes; }
+        else { minutesString = minutes.ToString(); }
+        if (seconds < 10) { secondsString = "0" + seconds; }
+        else { secondsString = seconds.ToString(); }
+
+        return "" + minutesString + ":" + secondsString;
+    }
+}
