e7b751bba019028638afa1c1ff7fcab67cbadd38
Dominick <d.meels@hotmail.com>
2016-11-23 22:25:23 +0100

    o Fixed the TimeHandler script to a nice clockish Timer

--- Assets/Game Folder/Scripts/ScoreAndTimerHandelr.cs
+++ Assets/Game Folder/Scripts/ScoreAndTimerHandelr.cs
@@ -20,6 +20,23 @@ public class ScoreAndTimerHandelr : MonoBehaviour
     {
         _team1_scoreText.text = "Team1: " + MatchStatistics.GetGoalsForTeam(1).ToString();
         _team2_scoreText.text = "Team2: " + MatchStatistics.GetGoalsForTeam(2).ToString();
-        _timerText.text = "Time: " + ((int)Time.time).ToString();
+        //_timerText.text = "Time: " + ((int)Time.time).ToString();
+        _timerText.text = transform2Clock((int)Time.time);
+    }
+
+    private string transform2Clock(int pTime)
+    {
+        int minutes = pTime / 60;
+        int seconds = pTime % 60;
+
+        string minutesString;
+        string secondsString;
+
+        if (minutes < 10) { minutesString = "0" + minutes; }
+        else { minutesString = minutes.ToString(); }
+        if (seconds < 10) { secondsString = "0" + seconds; }
+        else { secondsString = seconds.ToString(); }
+
+        return "Time: " + minutesString + ":" + secondsString;
     }
 }
