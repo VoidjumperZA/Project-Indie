0bbf7548c058f3cae665ed3a0e6be3fd523f07d7
Josh van Asten <joshmathew.va@gmail.com>
2016-11-25 20:42:59 +0100

    o Upgraded the pentagram's artwork o Stopped pentagrams spawning on columns with the Static type as well as those with the Locked status - Removed  manually place pentagrams from my scene

--- Assets/Game Folder/Scripts/PentagramInstantiator.cs
+++ Assets/Game Folder/Scripts/PentagramInstantiator.cs
@@ -19,16 +19,23 @@ public class PentagramInstantiator : MonoBehaviour
         GameObject[] columns = GameObject.FindGameObjectsWithTag("Column");
         for (int i = 0; i < columns.Length; i++)
         {
-            //instantiate our selection pentagram
-            GameObject newSelectionHex = Instantiate(selectionHexPrefab);
+            if (columns[i].GetComponent<ColumnProperties>().GetColumnType() == 0 && columns[i].GetComponent<ColumnProperties>().GetColumnStatus() == 0)
+            {
+                //instantiate our selection pentagram
+                GameObject newSelectionHex = Instantiate(selectionHexPrefab);
 
-            //parent it to the column
-            newSelectionHex.transform.SetParent(columns[i].transform);
-            newSelectionHex.transform.localScale = new Vector3(scalingSize, scalingSize, scalingSize);
-            newSelectionHex.transform.localPosition = new Vector3(0, 0, 0);
-            newSelectionHex.transform.Translate(0, columns[i].GetComponent<MeshRenderer>().bounds.extents.y * (2.0f + heightAdjustment), 0);
-            newSelectionHex.SetActive(false);
-            //0.037
+                //parent it to the column
+                newSelectionHex.transform.SetParent(columns[i].transform);
+
+                //scale it larger, zero it's position and translate it to the centre of the column
+                newSelectionHex.transform.localScale = new Vector3(scalingSize, scalingSize, scalingSize);
+                newSelectionHex.transform.localPosition = new Vector3(0, 0, 0);
+                newSelectionHex.transform.Translate(0, columns[i].GetComponent<MeshRenderer>().bounds.extents.y * (2.0f + heightAdjustment), 0);
+
+                //deactive it
+                newSelectionHex.SetActive(false);
+                //0.037
+            }
         }
     }
 
