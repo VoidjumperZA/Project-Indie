f8112b23519b802102ae655c42aabe00180b98da
Josh van Asten <joshmathew.va@gmail.com>
2016-11-25 20:26:49 +0100

    + Added a Prefabs folder + Added the PentagramInstantiator script, mean to dynamically create pentagrams at runtime instead of relying on them being placed manually over every column in every scene, by hand + Tried to add some FMOD stuff from the Greybox to my Testbench, hasn't give results. o Fixed the pentagrams. They now no longer break any scene that doesn't have them. Add PentagramInstantiator to your manager, drag in the prefab and give a scale value (0.037) and height adjustment (0.03) [these are for the greybox values, different scalings in different scenes might mean these should change, but that's why they're editable) o Cleared Chris' merge error

---
+++ Assets/Game Folder/Scripts/PentagramInstantiator.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PentagramInstantiator : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject selectionHexPrefab;
+
+    [SerializeField]
+    private float scalingSize;
+
+    [SerializeField]
+    private float heightAdjustment;
+
+    // Use this for initialization
+    void Start()
+    {
+        //grab all the columns in the scene
+        GameObject[] columns = GameObject.FindGameObjectsWithTag("Column");
+        for (int i = 0; i < columns.Length; i++)
+        {
+            //instantiate our selection pentagram
+            GameObject newSelectionHex = Instantiate(selectionHexPrefab);
+
+            //parent it to the column
+            newSelectionHex.transform.SetParent(columns[i].transform);
+            newSelectionHex.transform.localScale = new Vector3(scalingSize, scalingSize, scalingSize);
+            newSelectionHex.transform.localPosition = new Vector3(0, 0, 0);
+            newSelectionHex.transform.Translate(0, columns[i].GetComponent<MeshRenderer>().bounds.extents.y * (2.0f + heightAdjustment), 0);
+            newSelectionHex.SetActive(false);
+            //0.037
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
