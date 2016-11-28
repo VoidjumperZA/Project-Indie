5d91c71efb2f35e7fbf86112192e5b5fcaac6a93
Josh van Asten <joshmathew.va@gmail.com>
2016-11-28 22:28:04 +0100

    o Fixed the null reference stopping the match from starting

--- Assets/Game Folder/Scripts/MatchInitialisation.cs
+++ Assets/Game Folder/Scripts/MatchInitialisation.cs
@@ -29,6 +29,7 @@ public class MatchInitialisation : MonoBehaviour
 
         setCameraDimensions();
         assignPlayersToTeams();
+        setCorrectPlayerPositions();
         assignPlayerIDsAndRaycasts();
         passCrosshairsToUIManager();
     }
