d22570e09c8278409ab195de06146e64caa4294c
Josh van Asten <joshmathew.va@gmail.com>
2017-01-29 01:44:25 +0100

    o Super small start to "VoteToExitSubmenu.cs"

--- Assets/Game Folder/Scripts/VoteToExitSubmenu.cs
+++ Assets/Game Folder/Scripts/VoteToExitSubmenu.cs
@@ -1,16 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+public class VoteToExitSubmenu : MonoBehaviour
+{
+    [SerializeField]
+    private Image[] votersProfiles;
+    private int totalVotes;
+    private int currentVotes;
 
-public class VoteToExitSubmenu : MonoBehaviour {
+    private bool voteAxisLock;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        currentVotes = 0;
+        totalVotes = 4;
+        voteAxisLock = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkVotes();
+    }
+
+    private void checkVotes()
+    {
+        /*for (int i = 0; i < length; i++)
+        {
+
+        }*/
+        if (InputManager.MovementVertical(pauseScreenOwner) > 0 && voteAxisLock == false)
+        {
+            voteAxisLock = true;
+
+        }
+        else if (InputManager.MovementVertical(pauseScreenOwner) == 0)
+        {
+            voteAxisLock = false;
+        }
+    }
 }
