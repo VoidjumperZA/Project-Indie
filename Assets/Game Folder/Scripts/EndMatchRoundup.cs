83d3b56817e7f212ca8ca3ff22ca48ae3556554b
Josh van Asten <joshmathew.va@gmail.com>
2017-02-03 05:43:27 +0100

    no message

--- Assets/Game Folder/Scripts/EndMatchRoundup.cs
+++ Assets/Game Folder/Scripts/EndMatchRoundup.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class EndMatchRoundup : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject[] winPositions;
 
+    private PlayerInput[] playerInputs;
+    private ActivePlayers activePlayers;
     // Use this for initialization
     void Start()
     {
-
+        activePlayers = GameObject.Find("Manager").GetComponent<ActivePlayers>();
+        for (int i = 0; i < activePlayers.GetPlayersInMatchArraySize(); i++)
+        {
+            playerInputs[i] = activePlayers.GetPlayerInMatch(i).GetComponent<PlayerInput>();
+        }
     }
 
     // Update is called once per frame
